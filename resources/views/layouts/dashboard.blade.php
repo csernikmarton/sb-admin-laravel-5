03c66c4fbb1f328a2946ce35f3dcfa62d783b68e
Viktor Maár <viktor.maarsson@gmail.com>
2017-07-24 12:42:36 +0200

    Routing simplified

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -247,21 +247,21 @@
                         <li {{ (Request::is('/') ? 'class="active"' : '') }}>
                             <a href="{{ url ('') }}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
-                        <li {{ (Request::is('*charts') ? 'class="active"' : '') }}>
-                            <a href="{{ url ('charts') }}"><i class="fa fa-bar-chart-o fa-fw"></i> Charts</a>
+                        <li {{ (Request::is('*mcharts') ? 'class="active"' : '') }}>
+                            <a href="{{ url ('mcharts') }}"><i class="fa fa-bar-chart-o fa-fw"></i> Charts</a>
                             <!-- /.nav-second-level -->
                         </li>
-                        <li {{ (Request::is('*tables') ? 'class="active"' : '') }}>
-                            <a href="{{ url ('tables') }}"><i class="fa fa-table fa-fw"></i> Tables</a>
+                        <li {{ (Request::is('*table') ? 'class="active"' : '') }}>
+                            <a href="{{ url ('table') }}"><i class="fa fa-table fa-fw"></i> Tables</a>
                         </li>
-                        <li {{ (Request::is('*forms') ? 'class="active"' : '') }}>
-                            <a href="{{ url ('forms') }}"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                        <li {{ (Request::is('*form') ? 'class="active"' : '') }}>
+                            <a href="{{ url ('form') }}"><i class="fa fa-edit fa-fw"></i> Forms</a>
                         </li>
                         <li >
                             <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
-                                <li {{ (Request::is('*panels') ? 'class="active"' : '') }}>
-                                    <a href="{{ url ('panels') }}">Panels and Collapsibles</a>
+                                <li {{ (Request::is('*panel') ? 'class="active"' : '') }}>
+                                    <a href="{{ url ('panel') }}">Panels and Collapsibles</a>
                                 </li>
                                 <li {{ (Request::is('*buttons') ? 'class="active"' : '') }}>
                                     <a href="{{ url ('buttons' ) }}">Buttons</a>
--- routes/web.php
+++ routes/web.php
@@ -11,68 +11,7 @@
 |
 */
 
-Route::get('/', function()
-{
-	return view('home');
-});
-
-Route::get('/charts', function()
-{
-	return view('mcharts');
-});
-
-Route::get('/tables', function()
-{
-	return view('table');
-});
-
-Route::get('/forms', function()
-{
-	return view('form');
-});
-
-Route::get('/grid', function()
-{
-	return view('grid');
-});
-
-Route::get('/buttons', function()
-{
-	return view('buttons');
-});
-
-
-Route::get('/icons', function()
-{
-	return view('icons');
-});
-
-Route::get('/panels', function()
-{
-	return view('panel');
-});
-
-Route::get('/typography', function()
-{
-	return view('typography');
-});
-
-Route::get('/notifications', function()
-{
-	return view('notifications');
-});
-
-Route::get('/blank', function()
-{
-	return view('blank');
-});
-
-Route::get('/login', function()
-{
-	return view('login');
-});
-
-Route::get('/documentation', function()
-{
-	return view('documentation');
+Route::group(array('prefix' => '/'), function () {
+    Route::get('/', 'ThemeController@show' );
+    Route::get('/{page}', 'ThemeController@show' );
 });
